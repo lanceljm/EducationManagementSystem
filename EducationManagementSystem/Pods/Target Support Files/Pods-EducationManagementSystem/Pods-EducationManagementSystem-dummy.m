1f15a354abff6f059496c410567f6af038775dff
ljm <ljm@icloud.com>
2017-01-10 10:40:21 +0800

    判断登录

--- EducationManagementSystem/Pods/Target Support Files/Pods-EducationManagementSystem/Pods-EducationManagementSystem-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EducationManagementSystem : NSObject
-@end
-@implementation PodsDummy_Pods_EducationManagementSystem
-@end
--- EducationManagementSystem/Pods/Target Support Files/Pods-EducationManagementSystemUITests/Pods-EducationManagementSystemUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EducationManagementSystemUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EducationManagementSystemUITests
-@end
